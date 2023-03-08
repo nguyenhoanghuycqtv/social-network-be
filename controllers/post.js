5204fce91e31d06d182027a89ff9fd7ce6572675
Huy Nguyen <nguyenhoanghuycqtv@gmail.com>
2023-03-08 17:30:54 +0700

    user controller getUsers signup login

--- controllers/post.js
+++ controllers/post.js
@@ -1,16 +1,9 @@
-const { v4: uuidv4 } = require("uuid");
-
 const { validationResult } = require("express-validator");
 
 const HttpError = require("../models/http-error");
 
 const Post = require("../models/post");
 const post = require("../models/post");
-
-let DUMMY_POSTS = [
-  { id: "p1", title: "MU", content: "ABCXYZ", creator: "u1" },
-  { id: "p2", title: "MC", content: "ABCXYZ", creator: "u2" },
-];
 
 exports.getPostById = async (req, res, next) => {
   const postId = req.params.pid;
@@ -48,7 +41,9 @@ exports.getPostsByUserId = async (req, res, next) => {
 exports.createPost = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    throw new HttpError("Invalid input passed, please check your data", 422);
+    return next(
+      new HttpError("Invalid input passed, please check your data", 422)
+    );
   }
 
   const { title, content, creator } = req.body;
--- controllers/user.js
+++ controllers/user.js
@@ -1,42 +1,63 @@
-const { v4: uuidv4 } = require("uuid");
 const { validationResult } = require("express-validator");
 const HttpError = require("../models/http-error");
+const User = require("../models/user");
 
-DUMMY_USERS = [
-  {
-    id: "u1",
-    name: "Nguyen Hoang Huy",
-    email: "nguyenhoanghuycqtv@gmail.com",
-    password: "123456",
-  },
-];
-
-exports.getUsers = (req, res, next) => {
-  const users = [...DUMMY_USERS];
-
-  res.json({ users });
+exports.getUsers = async (req, res, next) => {
+  let users;
+  try {
+    users = await User.find({}, "-password");
+  } catch (err) {
+    return next("Could not fetch users data", 500);
+  }
+  res
+    .status(200)
+    .json({ users: users.map((user) => user.toObject({ getters: true })) });
 };
 
-exports.signup = (req, res, next) => {
+exports.signup = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    throw new HttpError("Invalid input, Please enter data again");
+    return next(new HttpError("Invalid input, Please enter data again", 422));
   }
   const { name, email, password } = req.body;
-  const hasUser = DUMMY_USERS.find((u) => u.email === email);
-  if (hasUser) {
-    throw new HttpError("Email has existed, please try different email");
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email });
+  } catch (err) {
+    return next(new HttpError("Signing Up failed, please try again", 500));
   }
-  const userCreated = { id: uuidv4(), name, email, password };
-  DUMMY_USERS.push(userCreated);
-  res.json({ user: userCreated });
+  if (existingUser) {
+    return next(new HttpError("User has existed", 422));
+  }
+
+  const userCreated = new User({
+    name,
+    email,
+    password,
+    image:
+      "https://scontent.fsgn3-1.fna.fbcdn.net/v/t39.30808-6/335087045_1840716722996808_8755302137692866446_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=0debeb&_nc_ohc=A31BBxPc0CEAX8T-teV&_nc_ht=scontent.fsgn3-1.fna&oh=00_AfBIqkp_kf4z8kWZWbbSaemBzTmqKu5kdiJmfYqHVCI7mw&oe=640D0284",
+    posts: "First post",
+  });
+
+  try {
+    await userCreated.save();
+  } catch (err) {
+    return next(new HttpError("Creating user failed, please try again.", 500));
+  }
+
+  res.status(201).json({ user: userCreated.toObject({ getters: true }) });
 };
 
-exports.login = (req, res, next) => {
+exports.login = async (req, res, next) => {
   const { email, password } = req.body;
-  const identifiedUser = DUMMY_USERS.find((u) => u.email === email);
-  if (!identifiedUser || identifiedUser.password !== password) {
-    throw new HttpError("Could not find provided email or password", 401);
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email });
+  } catch (err) {
+    return next(new HttpError("Logging In failed, please try again", 500));
+  }
+  if (!existingUser || existingUser.password !== password) {
+    return next(new HttpError("Logging In failed, please try again", 401));
   }
   res.json({ message: "Logged in" });
 };
