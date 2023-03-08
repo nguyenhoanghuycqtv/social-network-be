5183f821cc501f09d9926f81f7360b409696de7d
Huy Nguyen <nguyenhoanghuycqtv@gmail.com>
2023-03-08 16:32:43 +0700

    createPost deletePost getPostById getPostsByUserId

--- controllers/post.js
+++ controllers/post.js
@@ -5,35 +5,44 @@ const { validationResult } = require("express-validator");
 const HttpError = require("../models/http-error");
 
 const Post = require("../models/post");
+const post = require("../models/post");
 
 let DUMMY_POSTS = [
   { id: "p1", title: "MU", content: "ABCXYZ", creator: "u1" },
   { id: "p2", title: "MC", content: "ABCXYZ", creator: "u2" },
 ];
 
-exports.getPostById = (req, res, next) => {
+exports.getPostById = async (req, res, next) => {
   const postId = req.params.pid;
-  const post = DUMMY_POSTS.find((p) => {
-    return p.id === postId;
-  });
+  let post;
+  try {
+    post = await Post.findById(postId);
+  } catch (err) {
+    return next(new HttpError("Could not find a post", 500));
+  }
 
   if (!post) {
-    throw new HttpError("Could not find a post for the provided id", 404);
+    return next(
+      new HttpError("Could not find a post for the provided id", 404)
+    );
   }
-  res.json({ post });
+  res.json({ post: post.toObject({ getters: true }) });
 };
 
-exports.getPostsByUserId = (req, res, next) => {
+exports.getPostsByUserId = async (req, res, next) => {
   const userId = req.params.uid;
-  const posts = DUMMY_POSTS.filter((p) => {
-    return p.creator === userId;
-  });
+  let posts;
+  try {
+    posts = await Post.find({ creator: userId });
+  } catch (err) {
+    return next(new HttpError("Could not find posts of user", 500));
+  }
   if (!posts || posts.length === 0) {
     return next(
       new HttpError("Could not find posts for the provided userId", 404)
     );
   }
-  res.json({ posts });
+  res.json({ posts: posts.map((post) => post.toObject({ getters: true })) });
 };
 
 exports.createPost = async (req, res, next) => {
@@ -59,7 +68,7 @@ exports.createPost = async (req, res, next) => {
   res.status(201).json({ post: createdPost });
 };
 
-exports.updatePostById = (req, res, next) => {
+exports.updatePostById = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     throw new HttpError("Invalid input passed, please check your data", 422);
@@ -67,23 +76,36 @@ exports.updatePostById = (req, res, next) => {
 
   const { title, content } = req.body;
   const postId = req.params.pid;
-  const updatedPost = { ...DUMMY_POSTS.find((p) => p.id === postId) };
-  const postIndex = DUMMY_POSTS.findIndex((p) => p.id === postId);
-  updatedPost.title = title;
-  updatedPost.content = content;
-  DUMMY_POSTS[postIndex] = updatedPost;
-  //   DUMMY_POSTS[postIndex].title = title;
-  //   DUMMY_POSTS[postIndex].content = content;
-
-  res.status(200).json({ post: updatedPost });
-};
-
-exports.deletePostById = (req, res, next) => {
-  const postId = req.params.pid;
-  if (!DUMMY_POSTS.find((p) => p.id === postId)) {
-    throw new HttpError("Could not find the id provided", 404);
+  let post;
+  try {
+    post = await Post.findById(postId);
+  } catch (err) {
+    return next(
+      new HttpError("Something went wrong, could not update post", 500)
+    );
   }
 
-  DUMMY_POSTS = DUMMY_POSTS.filter((p) => p.id !== postId);
+  post.title = title;
+  post.content = content;
+
+  try {
+    post.save();
+  } catch (err) {
+    return next(
+      new HttpError("Something went wrong, could not update post", 500)
+    );
+  }
+
+  res.status(200).json({ post: post.toObject({ getters: true }) });
+};
+
+exports.deletePostById = async (req, res, next) => {
+  const postId = req.params.pid;
+  try {
+    await Post.findByIdAndRemove(postId);
+  } catch (err) {
+    return next(new HttpError("Could not delete post", 500));
+  }
+
   res.status(200).json("Deleted");
 };
